363ac093bc92e039dcb1ebb9be2f9dfb9ffd9f9e
Shyam <shyam.pgmr@gmail.com>
2017-05-30 18:49:10 +0530

    Feat: Create Controller file for Table, Collection Screen and Custom Cell for TableView

---
+++ MeMe-2.0/MMMemeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MMMemeTableViewCell.swift
+//  MeMe-2.0
+//
+//  Created by Shyam on 30/05/17.
+//  Copyright © 2017 Shyam. All rights reserved.
+//
+
+import UIKit
+
+class MMMemeTableViewCell: UITableViewCell {
+
+    // MARK: Outlet 
+    
+    @IBOutlet weak var memeImageView: UIImageView!
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
