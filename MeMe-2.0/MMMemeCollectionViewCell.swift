0600c0d6034a4bb7290d0150c8919947427e2850
Shyam <shyam.pgmr@gmail.com>
2017-05-30 18:52:08 +0530

    Feat: Create Custom Collection Cell for Collection screen

---
+++ MeMe-2.0/MMMemeCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MMMemeCollectionViewCell.swift
+//  MeMe-2.0
+//
+//  Created by Shyam on 30/05/17.
+//  Copyright © 2017 Shyam. All rights reserved.
+//
+
+import UIKit
+
+class MMMemeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var memeImageView: UIImageView!
+    
+    
+}
